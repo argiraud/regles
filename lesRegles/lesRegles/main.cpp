edec4055ae0efa5a5d61ab77908a6cddc47372c1
argiraud <a.giraud43@outlook.fr>
2018-03-16 20:31:34 -0400

    initialisation

---
+++ lesRegles/lesRegles/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lesRegles
+//
+//  Created by Arthur Giraud on 16/03/2018.
+//  Copyright © 2018 Arthur Giraud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
